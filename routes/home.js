fc734bdb50391bc978f575da7ee7abb859de35b9
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-07-07 14:54:14 +0200

    removed ticker and db connection

--- routes/home.js
+++ routes/home.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const app = express();
 
-const tickerController = require('../controllers/ticker');
-
 app.get('/', (req, res, next) => {
-    tickerController.get()
-    .then((tickerData) => {
-        res.render('./pages/index', {
-            ticker: tickerData
-        });
-    })
-    
+        res.render('./pages/index');
 });
 
 module.exports = app;
--- routes/investors.js
+++ routes/investors.js
@@ -1,16 +1,8 @@
 const express = require('express');
 const app = express();
 
-const tickerController = require('../controllers/ticker');
-
 app.get('/', (req, res, next) => {
-    tickerController.get()
-    .then((tickerData) => {
-        res.render('./pages/investors', {
-            ticker: tickerData
-        });
-    })
-    
+        res.render('./pages/investors');
 });
 
 module.exports = app;
