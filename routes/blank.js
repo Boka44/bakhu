17ea0abaed7d64773c922f31c61a383e81a74193
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-07-16 13:00:42 +0200

    added sub menu and blank pages

--- routes/blank.js
+++ routes/blank.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res, next) => {
-        res.render('./pages/contact');
+        res.render('./pages/blank');
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -12,6 +12,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 const home = require('./routes/home');
 const investors = require('./routes/investors');
 const team = require('./routes/team');
+const contact = require('./routes/contact');
+const blank = require('./routes/blank');
 
 app.use(bodyParser.json());
 
@@ -20,6 +22,8 @@ app.set('view engine', 'ejs');
 app.use('/', home);
 app.use('/investors', investors);
 app.use('/team', team);
+app.use('/contact', contact);
+app.use('/blank', blank);
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
