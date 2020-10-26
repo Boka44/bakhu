5d95ae83428875238f37921c7ee8029ad0eaf82f
Nicholas A Hrboka <nicholashrboka@gmail.com>
2020-10-26 10:01:14 +0100

    new version, hid submenus, few new oages

--- server.js
+++ server.js
@@ -14,16 +14,18 @@ const investors = require('./routes/investors');
 const team = require('./routes/team');
 const contact = require('./routes/contact');
 const blank = require('./routes/blank');
+const corpgov = require('./routes/corpgov');
 
 app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 
 app.use('/', home);
-app.use('/investors', investors);
+app.use('/news', investors);
 app.use('/team', team);
 app.use('/contact', contact);
 app.use('/blank', blank);
+app.use('/corpgov', corpgov);
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
